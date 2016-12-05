29ee8a448dc4475cedf1fd574d2b72c7326c30bb
Arthur Littmann <arthur.littmann@gmail.com>
2016-12-05 15:03:04 +0000

    skills added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'project/show'
 
   root to: 'pages#home'
+  get '/getintouch', to: 'pages#getintouch'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
