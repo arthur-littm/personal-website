37b4d8673ed6acaf5e411b3f985357ba07bf89ac
Arthur Littmann <arthur.littmann@gmail.com>
2016-12-15 23:25:23 +0000

    added github contribution

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
+require 'open-uri'
+
 class PagesController < ApplicationController
   def home
+    html_file = open("https://github.com/arthur-littm")
+    html_doc = Nokogiri::HTML(html_file)
+
+    @contribution = html_doc.search('.js-contribution-graph h2').text.split("in").first
+
   end
 end
