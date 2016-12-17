23b056ef202e7a6e7337755c06c4ccb07fb755b0
Arthur Littmann <arthur.littmann@gmail.com>
2016-12-17 00:07:27 +0000

    link to recom added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,10 @@ require 'open-uri'
 
 class PagesController < ApplicationController
   def home
-    html_file = open("https://github.com/arthur-littm")
-    html_doc = Nokogiri::HTML(html_file)
+    # html_file = open("https://github.com/arthur-littm")
+    # html_doc = Nokogiri::HTML(html_file)
 
-    @contribution = html_doc.search('.js-contribution-graph h2').text.split("in").first
-
+    # @contribution = html_doc.search('.js-contribution-graph h2').text.split("in").first
+    @contribution = "710 contributions this year"
   end
 end
