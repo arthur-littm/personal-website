4dad6ece27f66a69b91af907a1ebaf6423a7c99b
Arthur Littmann <arthur.littmann@gmail.com>
2016-12-21 13:14:13 +0000

    scraping github contrib

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,9 @@ require 'open-uri'
 
 class PagesController < ApplicationController
   def home
-    # html_file = open("https://github.com/arthur-littm")
-    # html_doc = Nokogiri::HTML(html_file)
+    html_file = open("https://github.com/arthur-littm")
+    html_doc = Nokogiri::HTML(html_file)
 
-    # @contribution = html_doc.search('.js-contribution-graph h2').text.split("in").first
-    @contribution = "710 contributions this year"
+    @contribution = html_doc.search('.js-contribution-graph h2').text.split("in").first
   end
 end
