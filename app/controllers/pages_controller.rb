676c6cd7d4b11de023c5f71a475aac8f5634b519
Arthur Littmann <arthur.littmann@gmail.com>
2016-12-24 10:35:44 +0000

    added top button

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,6 @@ class PagesController < ApplicationController
     html_file = open("https://github.com/arthur-littm")
     html_doc = Nokogiri::HTML(html_file)
 
-    @contribution = html_doc.search('.js-contribution-graph h2').text.split("in").first
+    @contribution = html_doc.search('.js-contribution-graph h2').text
   end
 end
