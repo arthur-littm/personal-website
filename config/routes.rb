f86b30c3186cd3159f35dd78c88b416aebd28fff
Arthur Littmann <arthur.littmann@gmail.com>
2017-01-12 09:43:05 +0000

    added art gallery project page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get '/likeminded', to: 'projects#likeminded'
   get '/cap-vauban', to: 'projects#capvauban'
   get '/gearup', to: 'projects#gearup'
+  get '/art-gallery', to: 'projects#artgallery'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
