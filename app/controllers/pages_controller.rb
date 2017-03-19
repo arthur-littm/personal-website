fda981173197eedc02857771bd632bfb9f11a1f2
Arthur Littmann <arthur.littmann@gmail.com>
2017-03-19 16:17:30 +0000

    added scraping for links to youtube videos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,18 @@ require 'open-uri'
 
 class PagesController < ApplicationController
   def home
-    html_file = open("https://github.com/arthur-littm")
-    html_doc = Nokogiri::HTML(html_file)
+    github = open("https://github.com/arthur-littm")
+    github_doc = Nokogiri::HTML(github)
 
-    @contribution = html_doc.search('.js-contribution-graph h2').text
+    @contribution = github_doc.search('.js-contribution-graph h2').text
+
+    youtube = open("https://www.youtube.com/channel/UCC6p0L9affF4y4iIxB5jWoQ")
+    youtube_doc = Nokogiri::HTML(youtube)
+    @video1 = youtube_doc.search('.yt-uix-sessionlink').text
+    links = youtube_doc.css('div.compact-shelf-content-container .yt-uix-sessionlink').map { |link| link['href'] }
+    @latest_videos = []
+    links.uniq.first(3).each do |link|
+      @latest_videos << link.split("=")[-1]
+    end
   end
 end
